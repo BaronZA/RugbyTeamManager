4414086d2534ad2f7f21b64143e0064482da5af4
Jasen Van Rooyen <jasenza@gmail.com>
2020-11-08 11:28:45 +0200

    Initial project creation

---
+++ RugbyTeamManager/Controllers/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RugbyTeamManager.Models;
+using System;
+using System.Linq;
+
+namespace RugbyTeamManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerController : ControllerBase
+    {
+        private readonly ILogger<PlayerController> _logger;
+
+        public PlayerController(ILogger<PlayerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public PlayerInfos GetAllPlayers()
+        {
+            return new PlayerInfos();
+        }
+    }
+}
